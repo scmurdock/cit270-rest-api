086f15b38e3dabbe1d80cc6460bc33bdcbc0afed
Sean Murdock <murdockse@byui.edu>
2022-03-21 13:21:28 -0600

    Add files

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const https = require('https');
 const fs = require('fs');
+const md5 = require('md5');
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const port = 4443;
-
+const port = 443;
 const app = express();
+
+let invalidLoginAttempts=0;
 
 app.use(express.static('public'));
 
@@ -17,11 +18,18 @@ app.get('/',(req,res)=>{
 
 app.post('/login', (req,res) =>{
     console.log(JSON.stringify(req.body));
-    if(req.body.userName =="seanmurdock" && req.body.password=="6f1ed002ab5595859014ebf0951522d9"){
-        res.send("Welcome!")
-    } else{
-        res.send("Who are you?");
+    if(invalidLoginAttempts>=5){
+        res.status(401);//unauthorized        
+        res.send("Max attempts reach");
     }
+    else if(req.body.userName =="seanmurdock" && md5(req.body.password)=="161ebd7d45089b3446ee4e0d86dbcf92"){//  P@ssw0rd
+        res.send("Welcome!")
+    }  else{
+        invalidLoginAttempts++;
+        console.log(invalidLoginAttempts+" invalid attempts");
+        res.status(401);//unauthorized
+        res.send("Who are you?");
+    } 
 });
 
 https.createServer({
